cbb09fda2a5029f2885686a4393369fdca5785e2
HackerAndCoder <100743043+HackerAndCoder@users.noreply.github.com>
2023-09-19 09:15:45 -0600

    added cars

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+
+pygame.init()
+
+# variables
+
+window = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('GTA')
+
+game_clock = pygame.time.Clock()
+
+def handle_events(events):
+    for event in events:
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+while True:
+    game_clock.tick(30)
+    window.fill((255, 255, 255))
+    pygame.display.flip()
+    
+    handle_events(pygame.event.get())
